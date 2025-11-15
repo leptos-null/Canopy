362f182348499b8ea0879cb8ac49d36cc463ac05
Leptos <leptos.0.null@gmail.com>
2025-11-15 13:00:14 -0800

    Add blank Xcode project for multi-platform app

---
+++ Canopy/CanopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanopyApp.swift
+//  Canopy
+//
+//  Created by Leptos on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CanopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
